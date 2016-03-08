71ace52d8184dc0747484b782a4929685fbf9b71
loba1 <lucaslobao@outlook.com>
2016-03-08 19:16:50 -0300

    Added files via upload

---
+++ Aula3/Controller.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+include "Math.php";
+
+$math = new Math();
+
+echo $math->calculateCircleArea($_POST["radius"]);
+echo "<br>";
+//$math1 = new Math();
+echo $math->calculaHipotenusa($_POST["Adj"],$_POST["Op"]);
---
+++ Aula3/Index.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<form action="Controller.php" method="post">
+	<input name="radius" type="text"/>
+	<input type="submit" value="Calcular"/>
+	<br><br><br><br>
+	<input name="Adj" type="text"/>
+	<input name="Op" type="text"/>
+	<input type="submit" value="Calcular"/>
+
+</form>
+
+</body>
+</html>
