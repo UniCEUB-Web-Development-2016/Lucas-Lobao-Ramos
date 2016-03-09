50787099500af765be09fe36af6fc9d53bb107ec
loba1 <lucaslobao@outlook.com>
2016-03-08 22:37:33 -0300

    Update newUser.php

--- login_facebook/newUser.php
+++ login_facebook/newUser.php
@@ -8,24 +8,43 @@ class Usuario{
 	private $birthDate;
 	
 	public function setName($name){
-	$this->name = name;
+	$this->name = $name;
 	}
 	public function setEmail($email){
-	$this->email = email;
+	$this->email = $email;
 	}
 	public function setPassword($pass){
-	$this->password = pass;
+	$this->password = $pass;
 	}
 	public function setbirthDate($dta){
-	$this->birthDate = dta;
+	$this->birthDate = $dta;
+	}
+	
+	public function getName(){
+	return $this->name;
+	}
+	
+	public function getEmail(){
+	return $this->email;
+	}
+	
+	public function getPassword(){
+	return $this->password;
+	}
+	
+	public function getbirthDate(){
+	return $this->birthDate;
 	}
 	
 	public function createTextArquive(){
-	$arquive = fopen($name.'.txt','w');
-	$fwrite($arquive, $name);
-	$fwrite($arquive, $email);
-	$fwrite($arquive, $password);
-	$fwrite($arquive, $birthDate);
+	$arquive = fopen(self::getName().'.txt','w');
+	fwrite($arquive, self::getName());
+	fwrite($arquive, "\r\n");
+	fwrite($arquive, self::getEmail());
+	fwrite($arquive, "\r\n");
+	fwrite($arquive, self::getPassword());
+	fwrite($arquive, "\r\n");
+	fwrite($arquive, self::getbirthDate());
 	fclose($arquive);
 	}
 }
