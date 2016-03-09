0fb517dbd471a73dbd297ef4d6cd50be205ae684
loba1 <lucaslobao@outlook.com>
2016-03-08 21:57:03 -0300

    Update controller.php

--- login_facebook/controller.php
+++ login_facebook/controller.php
@@ -1,8 +1,17 @@
 <?php
 
+include "newUser.php";
+new User = new newUser();
 $name = $_POST[];
 $email = $_POST[];
 $password = $_POST[];
 $birthDate = $_POST[];
 
 new User = new newUser();
+
+$user->setName($name);
+$user->setEmail($email);
+$user->setPassword($password);
+$user->setbirthDate($birthDate);
+
+$user->createTextArquive();
