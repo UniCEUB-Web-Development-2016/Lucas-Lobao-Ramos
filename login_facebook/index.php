74fc2e21923e7b4cb7c4e9a0dfcd9ef11cb7b2af
loba1 <lucaslobao@outlook.com>
2016-03-11 18:38:56 -0300

    Update index.php

--- login_facebook/index.php
+++ login_facebook/index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
+<head>
 <title>HTML Tutorial</title>
+</head>
 <body>
 <center>
 <form action="controller.php" method="post"> 
-    <input type="text" name="n" placeholder="Nome">
+    <input type="text" name="n" placeholder="Nome" required="">
 	<br>
-	<input type="text" name="e" placeholder="Email">
+	<input type="text" name="e" placeholder="Email" required="">
 	<br>
-	<input type="password" name="p" placeholder="Senha">
+	<input type="password" name="p" placeholder="Senha" required="">
 	<br>
-	<input type="text" name="b" placeholder="Data de Nascimento">
+	<input type="text" name="b" placeholder="Data de Nascimento" required="">
 	<br>
 	<input type="submit">
 </form>
