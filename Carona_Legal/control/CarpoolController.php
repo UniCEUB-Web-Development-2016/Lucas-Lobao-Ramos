782575607e7bb6a8dde8ebcbc6448f8b5a7d29cc
loba1 <lucaslobao@outlook.com>
2016-04-17 23:55:38 -0300

    Setting Model Classes

---
+++ Carona_Legal/control/CarpoolController.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once "model/Carpool.php";
+class CarpoolController{
+	
+}
--- Carona_Legal/control/ResourceController.php
+++ Carona_Legal/control/ResourceController.php
@@ -1,13 +1,20 @@
 <?php
 
 include_once "model/Request.php";
-//include_once "control/UserController.php";
+include_once "control/UserController.php";
+include_once "control/UberController.php";
+include_once "control/CarpoolController.php";
 
 class ResourceController{
 	
+	private $ResourceMap = [
+		"carpool" 	=> "CarpoolController",
+		"user"		=> "UserController",
+		"uber"		=> "UberController"
+	];
 
 	public function createResource($request){
-		
-		return $request->getResource();
+
+		echo "OLHA O RESOURCE MLK!! -> ".$request->getResource();
 	}
 }
