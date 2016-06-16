a02cf2698c4c38da169ef871e02012e2917c092b
loba1 <lucaslobao@outlook.com>
2016-06-16 00:23:19 -0300

    Fixing form and sql query for db

--- Carona_Legal/coolcarpool.sql
+++ Carona_Legal/coolcarpool.sql
@@ -25,6 +25,11 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `user`
 --
+create schema IF NOT EXISTS coolcarpool;
+USE coolcarpool;
+
+
+
 
 CREATE TABLE `user` (
   `id` mediumint(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -35,13 +40,13 @@ CREATE TABLE `user` (
   `rg` int(7) NOT NULL,
   `phoneNumber` int(11) NOT NULL,
   `birthDate` varchar(10) NOT NULL,
-  `driver` tinyint(1) NOT NULL,
-  `carplate` varchar(8) NOT NULL,
+  `driver` varchar(2) NOT NULL,
+  `carplate` varchar(10) NOT NULL,
   `pass` varchar(200) NOT NULL
  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `carpool` (
-  `id` mediumint(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `carpoolId` mediumint(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `cpf` varchar(15) NOT NULL,
   `sLatitude` varchar(15) NOT NULL,
   `sLongitude` varchar(15) NOT NULL,
@@ -51,19 +56,16 @@ CREATE TABLE `carpool` (
   `timee` varchar(10) NOT NULL,
   `sRange` varchar(5) NOT NULL,
   `dRange` varchar(5) NOT NULL,
-  `carpoolId` mediumint(5) NOT NULL,
-  `emptySeats` mediumint(5) NOT NULL,
-  `pass` varchar(200) NOT NULL
+  `emptySeats` mediumint(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`name`, `lastName`, `email`, `cpf`, `rg`, `phoneNumber`, `birthDate`, `driver`, `carplate`) VALUES
-('Tomate', 'vermelho', 'tomatinho@saladas.com', 2147483647, 123456, 2147483647, '02-02-2002', 0, 'null'),
-('Cebola', 'Roxa', 'cebolinha@roxa.com', 2147483647, 2222222, 2147483647, '01-01-1001', 0, 'null'),
-('Laranja', 'muitolaranja', 'laranja@suculenta.com', 2147483647, 11111111, 2147483647, '02-02-2000', 0, 'null'),
-('Alface', 'Verde', 'alface@saladas.com', 2147483647, 13131313, 22222222, '03-03-1994', 0, 'null'),
-('arroz', 'branco', 'naosoufruta@alimentos.com', 2147483647, 3333333, 2147483647, '04-12-2016', 0, 'null'),
-('Cebolaaaao', 'Roxa', 'cebolinha@roxa.com', 2147483647, 2222222, 2147483647, '01-01-1001', 0, 'null');
-
+INSERT INTO `user` (`name`, `lastName`, `email`, `cpf`, `rg`, `phoneNumber`, `birthDate`, `driver`, `carplate`, `pass`) VALUES
+('Tomate', 'vermelho', 'tomatinho@saladas.com', 2147483647, 123456, 2147483647, '02-02-2002', 'n', 'null', 'HARDP4SSW0RD'),
+('Cebola', 'Roxa', 'cebolinha@roxa.com', 2147483647, 2222222, 2147483647, '01-01-1001', 's', 'abcd1234', 'senhafass1lxD'),
+('Laranja', 'muitolaranja', 'laranja@suculenta.com', 2147483647, 11111111, 2147483647, '02-02-2000', 'n', 'null', 'senhafass1lxD'),
+('Alface', 'Verde', 'alface@saladas.com', 2147483647, 13131313, 22222222, '03-03-1994', 's', 'efgh-5678', 'senhafass1lxD'),
+('arroz', 'branco', 'naosoufruta@alimentos.com', 2147483647, 3333333, 2147483647, '04-12-2016', 'n', 'null', 'senhafass1lxD'),
+('Cebolaaaao', 'Roxa', 'cebolinha@roxa.com', 2147483647, 2222222, 2147483647, '01-01-1001', 'n', 'null', 'senhafass1lxD');
